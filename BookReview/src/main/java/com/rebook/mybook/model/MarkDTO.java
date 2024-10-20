a87e81b733b1f5a8a664446a5bd912dae543ac59
JaeGwon <chol6245@naver.com>
2024-10-21 00:57:48 +0900

    markDAO

---
+++ BookReview/src/main/java/com/rebook/mybook/model/MarkDTO.java
@@ -0,0 +1,12 @@
+package com.rebook.mybook.model;
+
+import lombok.Data;
+
+@Data
+public class MarkDTO {
+	private String membername;
+	private String score;
+	private String bookname;
+	private String cover;
+	
+}
